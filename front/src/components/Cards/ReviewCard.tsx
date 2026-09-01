1fa3493b8a8fefa239f0b87d9a4204f336e07261
27sssssss <vinniksemen4@gmail.com>
2026-09-01 03:46:35 -0400

    card_done

--- front/src/components/Cards/ReviewCard.tsx
+++ front/src/components/Cards/ReviewCard.tsx
@@ -3,14 +3,17 @@ import commas from '../../assets/Vector.svg'
 
 export default function ReviewCard(){
     return(
-        <div className="max-w-210 min-w-210 bg-[#FFBF00] text-[#00211C] border rounded-3xl flex-col">
-            <div className="px-5 py-3">
-            <img src={commas} alt="" className="h-6 w-6" />
-                <p>
-                    this is our new card!
-                </p>
-            
-            <img src={commas} alt="" className="right-0 h-6 w-6" />
+        <div className="max-w-130 min-w-130 bg-[#FFBF00] text-[#00211C] border rounded-3xl">
+            <div className="flex flex-col justify-between px-5 py-3 min-h-60">
+                <img src={commas} alt="" className="h-6 w-6 self-start" />
+                    <div className=''>
+                        <p className='text-3xl font-semibold' style={{ fontFamily: '"AzeretMono", sans-serif'}}>ALESSANDRO ROMANO</p>
+                        <p className='font-light'>
+                            Founder & CEO, Hospitality Group
+                        </p>
+
+                    </div>
+                <img src={commas} alt="" className="self-end h-6 w-6 " />
 
             </div>
 
